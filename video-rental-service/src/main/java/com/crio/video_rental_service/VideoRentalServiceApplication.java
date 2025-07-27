98477649dc3754905c103c79ef1a06afa8bdf923
urakshitha <rakshithau2015@gmail.com>
2025-07-27 21:41:49 +0530

    Initial set up

---
+++ video-rental-service/src/main/java/com/crio/video_rental_service/VideoRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crio.video_rental_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoRentalServiceApplication.class, args);
+	}
+
+}
---
+++ video-rental-service/src/test/java/com/crio/video_rental_service/VideoRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crio.video_rental_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
